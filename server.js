271cb0d224449c75d218d2b0ca12ba6abd5f0301
Priya Joshi <82715174+priyajoshi2297@users.noreply.github.com>
2024-04-22 15:23:19 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.status(200).send("Hello server is running").end();
+});
+
+app.get("/testing", (req, res) => {
+  res.status(200).send("Hello priyaaa is running").end();
+});
+
+// Start the server
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+  console.log("Press Ctrl+C to quit.");
+});
