1cc188463a95278e24019a2dc7c437f7360912ac
Priya Joshi <82715174+priyajoshi2297@users.noreply.github.com>
2024-04-22 15:28:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,40 @@
 const express = require("express");
+const axios = require("axios");
+const path = require("path");
 const app = express();
+const cheerio = require("cheerio");
 
-app.get("/", (req, res) => {
-  res.status(200).send("Hello server is running").end();
+const PORT = process.env.PORT || 5001;
+
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("/api/get-complete-article-list", async (req, res) => {
+  const article_urls = req.query.urls ? req.query.urls.split(",") : [];
+  let results = []; // Array to hold the results of each fetch
+  try {
+    for (const [index, url] of article_urls.entries()) {
+      try {
+        const response = await axios.get(url, { responseType: "text" });
+        const $ = cheerio.load(response.data);
+        const textContent = $("body").text().replace(/\s+/g, " ").trim();
+        results.push(`Article ${index + 1}: ${textContent}`); // Add successful fetch result
+      } catch (error) {
+        console.error(`Failed to fetch Article ${index + 1}:`, error);
+        results.push(`Failed to fetch Article ${index + 1}`); // Add error message on failure
+      }
+    }
+    res.send(results.join("\n\n")); // Send back concatenated results, separated by double newlines
+  } catch (error) {
+    console.error("Error handling request:", error);
+    res.status(500).send("Failed to process the request");
+  }
 });
 
-app.get("/testing", (req, res) => {
-  res.status(200).send("Hello priyaaa is running").end();
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/build/index.html"));
 });
 
-// Start the server
-const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
-  console.log("Press Ctrl+C to quit.");
+  console.log(`Server listening on port ${PORT}`);
 });
+
